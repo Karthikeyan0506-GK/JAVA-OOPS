b64834d3d734cf6cba5e8e4c43f7f39d2d7c231c
Karthikeyan0506-GK <karthikeyan2k0506@gmail.com>
2025-02-19 13:27:11 +0530

    Add files via upload

---
+++ DateTime.java
@@ -0,0 +1,14 @@
+import java.util.Date;
+
+public class DateTime {
+    public static void main(String[] args) {
+        Date currentDate = new Date();
+        System.out.println("Current date and time: " + currentDate);
+
+    
+        // Calendar calendar = Calendar.getInstance();
+        // calendar.set(2024, Calendar.DECEMBER, 31, 12, 0, 0); 
+        // Date specificDate = calendar.getTime();
+        // System.out.println("Specific date and time: " + specificDate);
+    }
+}
